f33eaaaf425473ae90bb2db3d8a3362c588f0314
Rune Skovbo Johansen <rune@runevision.com>
2014-02-14 23:34:25 +0100

    Preparing level editor.

---
+++ Project1/Assets/Scripts/Editor/LevelEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor (typeof (Level))]
+public class LevelEditor : Editor {
+
+	int selectedBrush = 0;
+	GameObject selectedBrushPrefab
+	{
+		get
+		{
+			if (selectedBrush < 1)
+				return null;
+			return (target as Level).brushPrefabs[selectedBrush-1];
+		}
+	}
+	
+	public override void OnInspectorGUI ()
+	{
+		Level level = target as Level;
+		
+		// Brush toolbar
+		string[] brushNames = new string[level.brushPrefabs.Count + 1];
+		brushNames[0] = "Empty";
+		for (int i=0; i<level.brushPrefabs.Count; i++)
+			brushNames[i+1] = level.brushPrefabs[i].name;
+		selectedBrush = GUILayout.Toolbar (selectedBrush, brushNames);
+		
+		DrawDefaultInspector ();
+	}
+	
+	public void OnSceneGUI () {
+		Vector2 mousePos = GUIToGridPoint (Event.current.mousePosition);
+		DrawCell (mousePos);
+		if (Event.current.type == EventType.MouseMove)
+			Event.current.Use ();
+	}
+	
+	void DrawCell (Vector3 pos) {
+		Handles.DrawLine (pos + new Vector3 (1,1,0) * 0.5f, pos + new Vector3 (-1,1,0) * 0.5f);
+		Handles.DrawLine (pos + new Vector3 (-1,1,0) * 0.5f, pos + new Vector3 (-1,-1,0) * 0.5f);
+		Handles.DrawLine (pos + new Vector3 (-1,-1,0) * 0.5f, pos + new Vector3 (1,-1,0) * 0.5f);
+		Handles.DrawLine (pos + new Vector3 (1,-1,0) * 0.5f, pos + new Vector3 (1,1,0) * 0.5f);
+	}
+	
+	Vector2 GUIToGridPoint (Vector2 guiPoint) {
+		Plane plane = new Plane (-Vector3.forward, Vector3.zero);
+		Ray ray = HandleUtility.GUIPointToWorldRay (guiPoint);
+		float dist = 0;
+		plane.Raycast (ray, out dist);
+		Vector2 point = ray.GetPoint (dist);
+		point.x = Mathf.Round (point.x);
+		point.y = Mathf.Round (point.y);
+		return point;
+	}
+}
---
+++ Project1/Assets/Scripts/Level.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Level : MonoBehaviour {
+	
+	public List<GameObject> brushPrefabs;
+	public List<GameObject> tiles;
+}
