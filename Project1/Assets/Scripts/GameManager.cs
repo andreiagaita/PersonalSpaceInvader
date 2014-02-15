83f12e1192d6f465da53dda756dad0cdbb467c1d
Elvis Alistar <ealis2001@gmail.com>
2014-02-15 09:59:52 +0100

    Alternative scoring system

--- Project1/Assets/Scripts/GameManager.cs
+++ Project1/Assets/Scripts/GameManager.cs
@@ -3,4 +3,7 @@ using System.Collections;
 
 public static class GameManager {
 	public static int playersAlive = 3;
+	public static int scorePlayer1 = 0;
+	public static int scorePlayer2 = 0;
+	public static int scorePlayer3 = 0;
 }
--- Project1/Assets/Scripts/PlayerBehaviour.cs
+++ Project1/Assets/Scripts/PlayerBehaviour.cs
@@ -15,10 +15,12 @@ public class PlayerBehaviour : MonoBehaviour {
 		playerAuraDistance = Vector3.Distance (enemyAura.position, transform.position);
 		if (playerAuraDistance < distanceLimit)
 		{
+			AdjustScore();
 			Debug.Log(GameManager.playersAlive);
 			if (GameManager.playersAlive > 2)
 			{
-				KillEnemy();
+				//KillEnemy();
+				//re-assign player colors
 			}
 			else
 			{
@@ -31,5 +33,24 @@ public class PlayerBehaviour : MonoBehaviour {
 	{
 		GameManager.playersAlive--;
 		DestroyImmediate(enemyAura.transform.root.gameObject);
+	}
+
+	void AdjustScore()
+	{
+		switch (gameObject.tag)
+		{
+			case "Player1" : GameManager.scorePlayer1++; break;
+			case "Player2" : GameManager.scorePlayer2++; break;
+			case "Player3" : GameManager.scorePlayer3++; break;
+		}
+		switch (enemyAura.transform.root.gameObject.tag)
+		{
+			case "Player1" : GameManager.scorePlayer1--; break;
+			case "Player2" : GameManager.scorePlayer2--; break;
+			case "Player3" : GameManager.scorePlayer3--; break;
+		}
+		Debug.Log ("P1: " + GameManager.scorePlayer1 +
+		           " P2: " + GameManager.scorePlayer2 +
+		           " P3: " + GameManager.scorePlayer3);
 	}
 }
