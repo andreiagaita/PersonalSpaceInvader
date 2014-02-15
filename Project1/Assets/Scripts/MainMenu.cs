719fcb260f92e4aa8a9cd5dcd80c3290fe064cbd
Andreia Gaita <shana@spoiledcat.net>
2014-02-15 14:42:01 +0100

    Main menu

---
+++ Project1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void DoMenu (string menu)
+	{
+		switch (menu) {
+		case "start":
+			Application.LoadLevel ("LevelTest");
+			break;
+		default:
+			break;
+		}
+	}
+}
