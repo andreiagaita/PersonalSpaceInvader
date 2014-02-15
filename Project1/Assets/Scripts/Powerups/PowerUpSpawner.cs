905ae09a887a96025fbce9fce9c3da02b1940093
Elvis Alistar <ealis2001@gmail.com>
2014-02-15 21:28:50 +0100

    PowerUpSpawner

---
+++ Project1/Assets/Scripts/Powerups/PowerUpSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+	public GameObject[] powerUps;
+	public float delayBetweenSpawns = 10f;
+	private float timeSinceLastSpawn = 0f;
+	private GameObject currentPowerUp;
+
+	void Update () {
+		timeSinceLastSpawn += Time.deltaTime;
+		if (!currentPowerUp && (timeSinceLastSpawn > delayBetweenSpawns))
+		{
+			var index = Random.Range (0, powerUps.Length);
+			currentPowerUp = Instantiate(powerUps[index], transform.position, Quaternion.identity) as GameObject;
+			timeSinceLastSpawn = 0f;
+		}
+	}
+}
