33a862a648d6405fb1917d768dcf2a6b496aa9f9
Rune Skovbo Johansen <rune@unity3d.com>
2014-02-16 00:35:11 +0100

    Create one scene per level and add new level 3.

--- Project1/Assets/Scripts/GameManager.cs
+++ Project1/Assets/Scripts/GameManager.cs
@@ -35,7 +35,9 @@ public class GameManager : MonoBehaviour {
 			}
 		}
 	}
-
+	
+	public GameObject playerPrefab;
+	public int playerCount = 4;
 	public GameObject soundManagerPrefab;
 	public GUIText scoreTextPrefab;
 	public int scoreToWin = 20;
@@ -83,7 +85,9 @@ public class GameManager : MonoBehaviour {
 
 	private void InitLevels ()
 	{
-		levels.Add ("LevelTest");
+		levels.Add ("Level1");
+		levels.Add ("Level2");
+		levels.Add ("Level3");
 	}
 
 	private void InitLevel ()
@@ -92,6 +96,9 @@ public class GameManager : MonoBehaviour {
 			return;
 
 		gameRoundEnded = false;
+		
+		for (int i=0; i<playerCount; i++)
+			Instantiate (playerPrefab);
 		SpawnPlayers ();
 		AssignTargets ();
 		
@@ -131,6 +138,7 @@ public class GameManager : MonoBehaviour {
 
 	public void AddPlayer (PlayerBehaviour player)
 	{
+		player.playerColor = (PlayerColor)players.Count;
 		players.Add (player);
 		if (PlayerCreated != null)
 			PlayerCreated (player);
@@ -242,7 +250,7 @@ public class GameManager : MonoBehaviour {
 
 	public void NextLevel ()
 	{
-		currentLevel = UnityEngine.Random.Range (0, levels.Count - 1);
+		currentLevel = UnityEngine.Random.Range (0, levels.Count);
 
 		if (LevelStart != null)
 			LevelStart ();
