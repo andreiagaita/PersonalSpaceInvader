0cc20885fe3aabea6019eaa5c59b07ab0839178a
Veli-Pekka Kokkonen <veli@unity3d.com>
2014-02-15 01:45:17 +0100

    [flatmates] Protype level updated

--- flatmates/Assets/Scripts/PersonalSpace.cs
+++ flatmates/Assets/Scripts/PersonalSpace.cs
@@ -12,8 +12,8 @@ public class PersonalSpace : MonoBehaviour {
 
 	private void OnTriggerExit2D(Collider2D hit)
 	{
-		// Inform both players that their personal space was invaded
+		// Inform both players that their personal space is not invaded anymore
 		Dispatcher.SendMessage(hit.transform.parent.name, "PersonalSpaceUninvaded");
 		Dispatcher.SendMessage(transform.parent.name, "PersonalSpaceUninvaded");
-	}
+	} 
 }
