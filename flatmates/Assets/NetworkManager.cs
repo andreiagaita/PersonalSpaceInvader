95c2fb726dcb9567e3f5cc3548674695ece99615
Leonardo Carneiro <leo@zueb.com.br>
2014-02-15 21:58:54 +0100

    Handshaking RPC

---
+++ flatmates/Assets/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+	public GameStateManager gameStateManager;
+
+	void OnJoinedRoom()
+	{
+		PhotonView photonView = PhotonView.Get(this);
+		PlayerInfo myself = gameStateManager.CreateMySelf (PhotonNetwork.player.ID);
+		photonView.RPC("SetPlayerInfo", PhotonTargets.Others, myself.Name, myself.Position,myself.Score);
+    }
+
+	void OnPhotonPlayerConnected(PhotonPlayer newPlayer)
+	{
+		gameStateManager.PlayerJoined (newPlayer.ID, "Player " + newPlayer.ID, Vector3.zero, Color.green, 0);
+		PhotonView photonView = PhotonView.Get(this);
+		photonView.RPC("SetPlayerInfo", PhotonPlayer.Find(newPlayer.ID), gameStateManager.currentPlayerInfo.Name, gameStateManager.currentPlayerInfo.Position, gameStateManager.currentPlayerInfo.Score);
+	}
+
+	[RPC]
+	private void SetPlayerInfo(string playerName, Vector3 position/*, Color color*/, int score, PhotonMessageInfo messageInfo)
+	{
+		Debug.Log("GotPlayerInfo:" + playerName);
+		gameStateManager.SetPlayerInfo(messageInfo.sender.ID, playerName, position, Color.green, score);
+	}
+}
--- flatmates/Assets/Scripts/PlayerInfo.cs
+++ flatmates/Assets/Scripts/PlayerInfo.cs
@@ -1,34 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public enum ViewerType
 {
-    None,
-    Game,
-    Player,
-    Spectator
+	None,
+	Game,
+	Player,
+	Spectator
 }
 
 public class PlayerInfo
 {
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public Vector3 Position { get; set; }
-    public Color Color { get; set; }
-    public int Score { get; set; }
-    public List<PickupObject> ObjectsOwned { get; set; }
-    public List<PickupObject> ObjectsPicked { get; set; }
+	public int ID { get; set; }
+	public string Name { get; set; }
+	public Vector3 Position { get; set; }
+	public Color Color { get; set; }
+	public int Score { get; set; }
+	public List<PickupObject> ObjectsOwned { get; set; }
+	public List<PickupObject> ObjectsPicked { get; set; }
 
-    public PlayerInfo(int id, string name, Vector3 position, Color color, int score)
-    {
-        ID = id;
-        Name = name;
-        Position = position;
-        Color = color;
-        Score = score;
+	public PlayerInfo(int id, string name, Vector3 position, Color color, int score)
+	{
+		ID = id;
+		Name = name;
+		Position = position;
+		Color = color;
+		Score = score;
 
-        ObjectsOwned = new List<PickupObject>();
-        ObjectsPicked = new List<PickupObject>();
-    }
+		ObjectsOwned = new List<PickupObject>();
+		ObjectsPicked = new List<PickupObject>();
+	}
 }
