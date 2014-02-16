64519cbdaac56b1effd29d904324fc315b6bb060
Rune Skovbo Johansen <rune@unity3d.com>
2014-02-16 13:04:03 +0100

    Slo mo round end.

--- Project1/Assets/Scripts/GameManager.cs
+++ Project1/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System;
@@ -226,6 +227,8 @@ public class GameManager : MonoBehaviour {
 				spawnIndex = i;
 			}
 		}
+		if (spawnIndex < 0)
+			return Vector3.one * -100;
 		return spawnPoints[spawnIndex].transform.position;
 	}
 	
@@ -239,7 +242,7 @@ public class GameManager : MonoBehaviour {
 			RemovePlayers ();
 			RemoveSpawnPoints();
 			RemovePowerUpLocations ();
-			GameEnd ();
+			StartCoroutine ("GameEnd");
 		}
 	}
 
@@ -265,11 +268,19 @@ public class GameManager : MonoBehaviour {
 		aurasPulsating = false;
 	}
 
-	void GameEnd ()
+	IEnumerator GameEnd ()
 	{
 		currentLevel = -1;
 		if (GameEnded != null)
 			GameEnded ();
+		
+		foreach (var player in players)
+			player.GetComponent<CharacterController> ().enabled = false;
+		
+		Time.timeScale = 0.10f;
+		yield return new WaitForSeconds (0.7f);
+		Time.timeScale = 1;
+		
 		Application.LoadLevel("EndGameMenu");
 	}
 
