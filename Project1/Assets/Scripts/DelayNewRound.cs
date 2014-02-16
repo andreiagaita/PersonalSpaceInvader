462cddc57964d4413d4841a530254e196ad3b3ad
Elvis Alistar <ealis2001@gmail.com>
2014-02-16 10:55:02 +0100

    Adjust levels

---
+++ Project1/Assets/Scripts/DelayNewRound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayNewRound : MonoBehaviour {
+
+	public float delay = 1f;
+	public GameObject newRoundMenu;
+	private float elapsedTime = 0;
+
+	void Awake ()
+	{
+		newRoundMenu.GetComponent<MenuHandler>().enabled = false;
+	}
+
+	void Update () {
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime > delay)
+		{
+			newRoundMenu.GetComponent<MenuHandler>().enabled = true;
+			Destroy (gameObject);
+		}
+	}
+}
