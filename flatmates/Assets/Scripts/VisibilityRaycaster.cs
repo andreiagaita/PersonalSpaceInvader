a25e468cbd719108585fc9e4dbf50746d356cb2d
Veli-Pekka Kokkonen <veli@unity3d.com>
2014-02-16 16:24:10 +0100

    visibility fix

--- flatmates/Assets/Scripts/VisibilityRaycaster.cs
+++ flatmates/Assets/Scripts/VisibilityRaycaster.cs
@@ -32,7 +32,7 @@ public class VisibilityRaycaster : MonoBehaviour
 				Dispatcher.SendMessage(player.gameObject.name, "PlayerIsVisible", m_Myself.ID);
 				Dispatcher.SendMessage(name, "DidSawPlayer", player.gameObject.name);
 			}
-			else if (!(player is ClientPlayerInfo))
+			else if ((m_Myself is ClientPlayerInfo) && !(player is ClientPlayerInfo))
 			{
 				player.gameObject.transform.Find("Sprite").GetComponent<SpriteRenderer>().enabled = false;
 				player.gameObject.transform.Find("2DLight").GetComponent<Light2D>().enabled = false;
